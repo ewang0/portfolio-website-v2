42da6a232994cf559045629945a27250b8b3f08f
Eric Wang <87143658+ewang0@users.noreply.github.com>
2025-04-19 23:47:26 -0400

    linting, rounded img corners

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import '../App.css'
-import { Badge } from './ui/badge'
+"use client";
+import React from "react";
+import "../App.css";
+import { Badge } from "./ui/badge";
+
 interface GalleryProps {
-    selectedImgUrl: string | undefined,
-    skills: string[],
-    link: string
+  selectedImgUrl: string | undefined;
+  skills: string[];
+  link: string;
 }
 
 const Gallery: React.FC<GalleryProps> = ({ selectedImgUrl, skills, link }) => {
   return (
-    <div className='pb-5 md:grid grid-cols-1 w-full'>
-      <a className='cursor-pointer' href={link} target="_blank" rel="noopener noreferrer">
-        <div className='relative'>
-          <div className='overlay absolute inset-0 z-0' />
-            <img src={selectedImgUrl} className='w-full' alt='' draggable={false} />
+    <div className="pb-5 md:grid grid-cols-1 w-full">
+      <a
+        className="cursor-pointer"
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="relative">
+          <div className="overlay absolute inset-0 z-0" />
+          <img
+            src={selectedImgUrl || ""}
+            className="w-full rounded-xs"
+            alt=""
+            draggable={false}
+          />
         </div>
       </a>
-      <div className='flex flex-wrap gap-2 mt-5'>
+      <div className="flex flex-wrap gap-2 mt-5">
         {skills?.map((skill) => (
-          <Badge key={skill} variant="secondary" className="bg-neutral-200 text-neutral-900 dark:bg-neutral-800 dark:text-white">{skill}</Badge>
+          <Badge
+            key={skill}
+            variant="secondary"
+            className="bg-neutral-200 text-neutral-900 dark:bg-neutral-800 dark:text-white"
+          >
+            {skill}
+          </Badge>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
